b1bdca112364a7f930e74fb092860643f2ddcd6a
尚武忠 <18616749661@163.com>
2020-06-03 23:12:12 +0800

    week08总结+作业

--- week08/NOTE.md
+++ week08/NOTE.md
@@ -1,1 +1,162 @@
-# 每周总结可以写在这里
+## 选择器语法
+
+#### 简单选择器
+
+- *
+- div svg|a    namespace xml相关的知识
+- .cls
+- #id
+- [attr=value]
+- :hover    伪类选择器
+- ::before    伪元素选择器
+
+#### 复合选择器
+
+- <简单选择器><简单选择器><简单选择器>
+- *或者div必须写在最前面
+
+#### 复杂选择器
+
+- <复合选择器><sp><复合选择器>     子孙关系
+- <复合选择器>">"<复合选择器>    子选择器，严格的父子关系
+- <复合选择器>"~"<复合选择器>    
+- <复合选择器>"+"<复合选择器>
+- <复合选择器>"||"<复合选择器>    table 里面选中一列
+
+#### 选择器列表
+
+> 多个选择器用逗号隔开
+
+## 选择器优先级
+
+#### 简单选择器计数
+
+> [a, b, c, d]
+>
+> 按权重高低排
+>
+> 行内样式 <div style="xxx"></div> ==> a
+>
+> ID 选择器 ==> b
+>
+> 类，属性选择器和伪类选择器 ==> c
+>
+> 标签选择器，伪元素 ==> d
+
+#id div.a #id [0, 2, 1, 1];
+
+S = 0 * N³ + 2 * N² + 1 * N + 1
+
+取 N = 1000000
+
+S = 2000001000001
+
+## 伪类
+
+#### 链接/行为
+
+- :any-link
+- :link :visited
+- :hover
+- :active
+- :focus
+- :target
+
+#### 树结构
+
+- :empty
+- :nth-child()
+- :nth-last-child()
+- :first-child :last-child :only-child
+
+#### 逻辑型
+
+- :not
+- :where :has
+
+## 伪元素
+
+- ::before
+- ::after
+- ::firstLine
+  - font
+  - color
+  - ackground
+  - word-spacing
+  - letter-spacing
+  - text-decoration
+  - text-transform
+  - line-height
+- ::firstLetter
+  - font
+  - color
+  - background
+  - word-spacing
+  - letter-spacing
+  - text-decoration
+  - text-transform
+  - line-height
+  - vertical-align
+  - 盒模型系列：margin，padding，border
+
+## 排版
+
+#### 盒(Box)
+
+源代码	标签 Tag
+
+语义	元素 Element
+
+表现	盒 Box
+
+>HTML代码中可以书写开始<u>标签</u>，结束<u>标签</u>，和自封闭<u>标签</u>
+>
+>一对起止<u>标签</u>，表示一个<u>元素</u>
+>
+>DOM树中存储的是<u>元素</u>和其他类型的<u>节点</u>(Node)
+>
+>CSS选择器选中的是<u>元素</u>
+>
+>CSS选择器选中的元素，在排版时可能产生多个<u>盒</u>
+>
+>排版和渲染的基本单位是<u>盒</u>
+
+#### 盒模型
+
+margin ==> border ==> padding ==> content
+
+> width 一般表示content-box，需要添加 box-sizing:border 来修正
+
+box-sizing:
+
+- content-box
+- border-box
+
+## 正常流（normal-flow)
+
+#### 正常流排版
+
+- 收集盒进行
+- 计算盒在行中的排布
+- 计算行的排布
+
+#### 正常流的行模型
+
+Inline-block 建议 vertical-align 只用 top，middle，bottom
+
+> $0.getClientRects()
+
+#### float和clear
+
+#### margin折叠
+
+> Block formatting contexts
+
+#### overflow:visible 与 BFC
+
+## Flex
+
+- 收集盒进行
+- 计算盒在主轴方向的排布
+- 计算盒在交叉轴方向的排布
+
---
+++ week08/match.js
@@ -0,0 +1,115 @@
+function matchByClassSelector(selector, element) {
+    return element.className.split(/\s+/g).includes(selector.replace('.', ''))
+  }
+  
+  function matchByTypeSelector(selector, element) {
+    return element.tagName === selector.toUpperCase()
+  }
+  
+  function matchByIdSelector(selector, element) {
+    return element.id === selector.replace('#', '')
+  }
+  
+  // 属性值比较函数
+  const attrValueCompareFuns = {
+    '=': (attrValue, value) => attrValue === value,
+    '~=': (attrValue, value) => attrValue.split(/\s+/g).includes(value),
+    '|=': (attrValue, value) => attrValue === value || attrValue.startsWith(`${value}-`),
+    '^=': (attrValue, value) => attrValue.startsWith(value),
+    '$=': (attrValue, value) => attrValue.endsWith(value),
+    '*=': (attrValue, value) => attrValue.includes(value),
+  }
+  
+  function matchByAttributeSelector(selector, element) {
+    //                     key         comparetor    value
+    const match = /^\[\s*([\w-]+)\s*(?:([~|^$*]?=)\s*(\S+))?\s*\]$/.exec(selector)
+    if (!match) {
+      return false
+    }
+    // 属性名比较
+    const name = match[1]
+    const attrValue = element.getAttribute(name)
+    if (attrValue === null) {
+      return false
+    }
+    // 没有比较符号就没有属性值的比较
+    const comparator = match[2]
+    if (!comparator) {
+      return true
+    }
+    // 属性值比较
+    const value = match[3].replace(/["']/g, '') // 去除value的引号
+    return attrValueCompareFuns[comparator](attrValue, value)
+  }
+  
+  // 检查一个元素和简单选择器是否匹配
+  function matchBySimpleSelector(selector, element) {
+    if (!selector || !element) {
+      return false
+    } else if (selector.startsWith('#')) { // HASH
+      return matchByIdSelector(selector, element)
+    } else if (selector.startsWith('.')) { // class
+      return matchByClassSelector(selector, element)
+    } else if (selector.match(/^\[(.+?)\]$/)) { // attrib
+      return matchByAttributeSelector(selector, element)
+    } else if (selector.match(/^:not\((.+)\)$/)) { // negation
+      return !matchBySimpleSelectorSequence(element, RegExp.$1)
+    } else { // type_selector  
+      return matchByTypeSelector(selector, element)
+    }
+  }
+  
+  // 检查一个元素和简单选择器序列是否全匹配
+  function matchBySimpleSelectorSequence(simpleSelectorSequence, element) {
+    if (!simpleSelectorSequence || !element) {
+      return false
+    }
+    // `a#id.link[src^="https"]:not([targer='_blank'])` -> ["a", "#id", ".link", "[src^="https"]", ":not([targer='_blank'])"]
+    const simpleSelectors = simpleSelectorSequence.split(/(?<=[\w\]\)])(?=[#.:\[])/)
+    return simpleSelectors.every(simpleSelector => matchBySimpleSelector(simpleSelector, element))
+  }
+  
+  // 获取下一个待考查的元素
+  function getNextElementKey(combinator) {
+    return {
+      '>': 'parentElement',
+      ' ': 'parentElement',
+      '+': 'previousElementSibling',
+      '~': 'previousElementSibling',
+    }[combinator]
+  }
+  
+  // 查找一个与选择器匹配的element
+  function findMatchedElement(selector, element) {
+    if (!selector || !element) {
+      return null
+    }
+    const combinator = /[>+ ~]$/.test(selector) ? selector[selector.length - 1] : ''
+    const nextElementKey = getNextElementKey(combinator)
+  
+    if (/[>+]$/.test(selector)) {  // Child combinator OR Next-sibling combinator
+      selector = selector.replace(/[>+]$/, '')
+      element = element[nextElementKey]
+      if (!matchBySimpleSelectorSequence(selector, element)) {
+        element = null
+      }
+    } else if (/[ ~]$/.test(selector)) {  // Descendant combinator OR Subsequent-sibling combinator
+      selector = selector.replace(/[ ~]$/, '')
+      do {
+        element = element[nextElementKey]
+      } while (element && !matchBySimpleSelectorSequence(selector, element))
+    } else if (!matchBySimpleSelectorSequence(selector, element)) { // 唯一没有combinator的当前元素
+      element = null
+    }
+    return element || null
+  }
+  
+  // 检查一个元素和一个选择器是否匹配
+  function match(selector, element) {
+    // 'body  #form > .form-title  ~ label +  [role]' -> ["body ", "#form>", ".form-title~", "label+", "[role]"]
+    const selectors = rule.trim().replace(/(?<=[~+>])\s+/g, '').replace(/\s+(?=[ ~+>])/g, '').split(/(?<=[ ~+>])/g)
+    while (element && selectorParts.length) {
+      element = findMatchedElement(selectorParts.pop(), element)
+    }
+    return !!element
+  }
